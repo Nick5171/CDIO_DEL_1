bc9bd8b574b0740b66a079216036ee979737dcdd
Joes <s224564@student.dtu.dk>
2022-09-27 18:41:20 +0200

    Init_var first commit

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,16 @@
 public class Game {
+    public static void main(String[] args) {
+
+        Die die1, die2;
+        die1 = new Die();
+        die2 = new Die();
+        int points_P1 = 0;
+        int points_P2 = 0;
+        int a = 0;
+        int turn = 1;
+        boolean pair;
+        int sum;
+
+
+    }
 }
