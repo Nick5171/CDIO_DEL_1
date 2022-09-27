45f1cfab78ceaecff6c8327919deaa97d9dbbd3d
Nicklas Hovgaard <nhl0110@gmail.com>
2022-09-27 17:50:06 +0200

    Die class first commit

--- src/Die.java
+++ src/Die.java
@@ -1,2 +1,39 @@
-public class Die {
+public class Die
+{
+
+// Max face value
+
+private final int MAX = 6;
+
+    // Sets initial face value
+
+private int faceValue; // Value of die showing
+
+public Die() {faceValue = 1;}
+
+// Rolls the die and returns the result
+
+    public int roll()
+    {
+        faceValue = (int)(Math.random() * MAX) + 1;
+
+        return faceValue;
+    }
+
+// Face value mutator
+
+    public void setFaceValue (int value) { faceValue = value; }
+
+// Face value accessor
+
+    public int getFaceValue() { return faceValue; }
+
+// Returns a string representation of this die.
+
+    public String toString()
+    {
+        String result = Integer.toString(faceValue);
+
+        return result;
+    }
 }
