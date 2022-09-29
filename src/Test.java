5382fa9881df2fd45d932551cf431963c0f120ca
Philip <philip.tran@hotmail.dk>
2022-09-29 19:06:50 +0100

    Test commit - Cleaned up test class - changed games to throws

--- src/Test.java
+++ src/Test.java
@@ -8,9 +8,7 @@ public class Test {
         die1 = new Die();
         die2 = new Die();
         int points_P1 = 0; // Points for player 1
-        int points_P2 = 0; // Points for player 2
         int a = 0;
-        int turn = 1; //Turn counter
         boolean pair;
         int sum;
         int sum2 =0;
@@ -26,7 +24,7 @@ public class Test {
         int sum12 =0;
         int isPair = 0;
         double i;
-        int Throws = 0;
+        double Throws = 0;
 
 
         for( i = 0; i < 100000; i++) {
@@ -39,20 +37,10 @@ public class Test {
                 pair= getPair(die1.getFaceValue(), die2.getFaceValue());
                 sum = getSum(die1.getFaceValue(), die2.getFaceValue());
                 points_P1 = sum + points_P1;
-                if (points_P1 >= 40)
+                if (points_P1 >= 40){
                     a=1;
-
-                //Player 2's turn
-                die1.roll();
-                die2.roll();
-                Throws++;
-                pair= getPair(die1.getFaceValue(), die2.getFaceValue());
-                sum = getSum(die1.getFaceValue(), die2.getFaceValue());
-                points_P2 = sum + points_P2;
-                turn++;
-
-                if (points_P2 >= 40) // Stops while loop if one player has 40 or more points
-                    a = 1;
+                    points_P1 = 0;
+                }
 
                 if (pair) // Counts if dice thrown is a pair or not
                     isPair++;
@@ -98,19 +86,19 @@ public class Test {
         }
         System.out.println("Total throws: " + Throws);
         System.out.println("Over the course of " + i + " games played:");
-        System.out.println("The sum of 2 is thrown " + sum2 + " times which makes up " + df.format((sum2/i)*100) + "% of the total games played");
-        System.out.println("The sum of 3 is thrown " + sum3 + " times which makes up " + df.format((sum3/i)*100) + "% of the total games played");
-        System.out.println("The sum of 4 is thrown " + sum4 + " times which makes up " + df.format((sum4/i)*100) + "% of the total games played");
-        System.out.println("The sum of 5 is thrown " + sum5 + " times which makes up " + df.format((sum5/i)*100) + "% of the total games played");
-        System.out.println("The sum of 6 is thrown " + sum6 + " times which makes up " + df.format((sum6/i)*100) + "% of the total games played");
-        System.out.println("The sum of 7 is thrown " + sum7 + " times which makes up " + df.format((sum7/i)*100) + "% of the total games played");
-        System.out.println("The sum of 8 is thrown " + sum8 + " times which makes up " + df.format((sum8/i)*100) + "% of the total games played");
-        System.out.println("The sum of 9 is thrown " + sum9 + " times which makes up " + df.format((sum9/i)*100) + "% of the total games played");
-        System.out.println("The sum of 10 is thrown " + sum10 + " times which makes up " + df.format((sum10/i)*100) + "% of the total games played");
-        System.out.println("The sum of 11 is thrown " + sum11 + " times which makes up " + df.format((sum11/i)*100) + "% of the total games played");
-        System.out.println("The sum of 12 is thrown " + sum12 + " times which makes up " + df.format((sum12/i)*100) + "% of the total games played");
+        System.out.println("The sum of 2 is thrown " + sum2 + " times which makes up " + df.format((sum2/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 3 is thrown " + sum3 + " times which makes up " + df.format((sum3/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 4 is thrown " + sum4 + " times which makes up " + df.format((sum4/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 5 is thrown " + sum5 + " times which makes up " + df.format((sum5/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 6 is thrown " + sum6 + " times which makes up " + df.format((sum6/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 7 is thrown " + sum7 + " times which makes up " + df.format((sum7/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 8 is thrown " + sum8 + " times which makes up " + df.format((sum8/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 9 is thrown " + sum9 + " times which makes up " + df.format((sum9/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 10 is thrown " + sum10 + " times which makes up " + df.format((sum10/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 11 is thrown " + sum11 + " times which makes up " + df.format((sum11/Throws)*100) + "% of the total throws made");
+        System.out.println("The sum of 12 is thrown " + sum12 + " times which makes up " + df.format((sum12/Throws)*100) + "% of the total throws made");
         System.out.println();
-        System.out.println("Pairs have been thrown " + isPair + " times which makes up " + df.format((isPair/i)*100) + "% of the total games played");
+        System.out.println("Pairs have been thrown " + isPair + " times which makes up " + df.format((isPair/Throws)*100) + "% of the total throws made");
 
 
 
